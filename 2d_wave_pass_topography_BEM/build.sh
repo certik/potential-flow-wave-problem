d27908abd8b6b29b9ac4fc9753becf824c868e80
Ondřej Čertík <ondrej@certik.us>
2021-09-03 17:39:45 -0600

    Add optimized flags

--- 2d_wave_pass_topography_BEM/build.sh
+++ 2d_wave_pass_topography_BEM/build.sh
@@ -2,4 +2,6 @@
 
 set -ex
 
+#gfortran -O3 -march=native -ffast-math -funroll-loops wavebar.f90
+
 gfortran -Wall -Wextra -Wimplicit-interface -fPIC -fmax-errors=1 -g -fcheck=all -fbacktrace -Wno-unused-dummy-argument -Wno-unused-variable -Wno-conversion -Wno-compare-reals -Wno-tabs -Wno-unused-function  wavebar.f90
