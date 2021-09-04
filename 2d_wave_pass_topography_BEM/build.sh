bc525d9c422314a6e70aa5f8940f37a77ae7b067
Ondřej Čertík <ondrej@certik.us>
2021-09-03 18:16:43 -0600

    Disable warning

--- 2d_wave_pass_topography_BEM/build.sh
+++ 2d_wave_pass_topography_BEM/build.sh
@@ -4,4 +4,4 @@ set -ex
 
 #gfortran -O3 -march=native -ffast-math -funroll-loops wavebar.f90
 
-gfortran -Wall -Wextra -Wimplicit-interface -fPIC -fmax-errors=1 -g -fcheck=all -fbacktrace -Wno-unused-dummy-argument -Wno-unused-variable -Wno-conversion -Wno-compare-reals -Wno-tabs -Wno-unused-function  wavebar.f90
+gfortran -Wall -Wextra -Wimplicit-interface -fPIC -fmax-errors=1 -g -fcheck=all -fbacktrace -Wno-unused-dummy-argument -Wno-unused-variable -Wno-conversion -Wno-compare-reals -Wno-tabs -Wno-unused-label -Wno-unused-function  wavebar.f90
